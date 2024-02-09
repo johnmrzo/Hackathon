5def5cee54e688512171e93c14c1dd4eb6b73117
Bakhtiyor Mirzo <82373830+bakhtiyormirzo@users.noreply.github.com>
2024-02-09 18:29:14 +0300

    starter code

---
+++ Hackathon/HackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonApp.swift
+//  Hackathon
+//
+//  Created by Bakhtiyorjon Mirzajonov on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
